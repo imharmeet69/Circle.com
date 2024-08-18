a9deb05dfb4d8dba3a4eddd8cb925b34e38eb382
Harmeet Singh <164763075+imharmeet69@users.noreply.github.com>
2024-08-18 18:59:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const textBox = document.getElementById("textBox");
 const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelsius = document.getElementById("toCelsius");
+const Area = document.getElementById("Area");
 const result = document.getElementById("result");
 let temp;
 
@@ -20,8 +21,18 @@ function convert () {
     setTimeout(() => {
       result.textContent = "";
     }, 2000);
-  } else {
+  } else if (Area.checked) {
+    temp = Number(textBox.value);
+    temp = Math.PI * Math.pow(temp, 2);
+    result.textContent = temp.toFixed(1) + 	"cm";
+    setTimeout(() => {
+      result.textContent = "";
+    }, 2000);
+  } 
+
+  else {
     result.textContent = "Select a unit";
   }
 }
+
 
